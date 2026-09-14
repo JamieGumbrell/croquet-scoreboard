ec6d745b2170127d0e9982ba3211c7611fb8c63f
JamieGumbrell <jamie@gumbrell.net>
2026-09-14 21:05:08 +1000

    Laravel setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Croquet Scoreboard</title>
+</head>
+<body>
+    <h1>Croquet Scoreboard</h1>
+</body>
+</html>
